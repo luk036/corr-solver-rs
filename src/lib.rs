13a3cba993d4fd02d003abaa823be3ad13ae9c6c
Wai-Shing Luk <luk036@gmail.com>
2022-08-12 17:04:39 +0800

    added a test

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
 // todo
 pub mod ldlt_ext;
+pub use ldlt_ext::LDLTMgr;
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ndarray::prelude::*;
+
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn test_ldlt1() {
+        let m1 = array![[25.0, 15.0, -5.0], [15.0, 18.0, 0.0], [-5.0, 0.0, 11.0]];
+        let mut mgr1 = LDLTMgr::new(3);
+        assert!(mgr1.factorize(&m1));
+    }
+}
